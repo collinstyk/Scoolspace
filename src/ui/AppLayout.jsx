dc2c9dc27171915a8f0933630817ee1f07062028
collinstyk <mattycollins803@gmail.com>
2024-11-21 18:47:41 +0100

    Update all pages

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -4,7 +4,7 @@ import Footer from "./Footer";
 function AppLayout() {
   return (
     <>
-      <main className="w-screen bg-white font-plus-jakarta-sans">
+      <main className="h-screen w-screen bg-white font-plus-jakarta-sans">
         <Outlet />
         <Footer />
       </main>
